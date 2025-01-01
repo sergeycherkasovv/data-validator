58bf9ba6d141cfeca69fc87e8a0941d59ef603fc
sergeycherkasovv <iamcherkasov.job@gmail.com>
2025-01-01 16:53:00 +0700

    commit Universion Files

---
+++ app/build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    id("java")
+    application
+    checkstyle
+    jacoco
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+application {
+    mainClass = "hexlet.code.App"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.18.0")
+    compileOnly("org.projectlombok:lombok:1.18.34")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.18.0")
+    implementation("info.picocli:picocli:4.7.6")
+    annotationProcessor("info.picocli:picocli-codegen:4.7.6")
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport) // report is always generated after tests run
+}
+tasks.jacocoTestReport {
+    dependsOn(tasks.test) // tests are required to run before generating the report
+    reports { xml.required.set(true) }
+}
