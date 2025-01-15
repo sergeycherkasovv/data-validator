0e7a6c9a5c7ba5b3ffda8d3b7e7e03cd5d39f7e3
sergeycherkasovv <iamcherkasov.job@gmail.com>
2025-01-16 01:03:57 +0700

    changed NumberSchemaTest.java

--- app/src/test/java/hexlet/code/schemas/NumberSchemaTest.java
+++ app/src/test/java/hexlet/code/schemas/NumberSchemaTest.java
@@ -4,6 +4,7 @@ import hexlet.code.Validator;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
