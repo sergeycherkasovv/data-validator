7cc6c0b43060a3269b12e0c0fed7debc4b6db9b5
sergeycherkasovv <iamcherkasov.job@gmail.com>
2025-01-15 20:06:53 +0700

    add StringSchemaTest.java

---
+++ app/src/test/java/hexlet/code/schemas/StringSchemaTest.java
@@ -0,0 +1,64 @@
+package hexlet.code.schemas;
+
+import hexlet.code.Validator;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class StringSchemaTest {
+    StringSchema schema;
+
+    @BeforeEach
+    void beforeEach() {
+        var valid = new Validator();
+        schema = valid.string();
+    }
+
+    @Test
+    void requiredTest() {
+        assertTrue(schema.isValid(null));
+        schema.required();
+
+        assertFalse(schema.isValid(null));
+        assertTrue(schema.isValid(""));
+        assertTrue(schema.isValid("notNull"));
+    }
+
+    @Test
+    void minLengthTest() {
+        schema.minLength(6);
+
+        assertFalse(schema.isValid("fail"));
+
+        assertTrue(schema.isValid(null));
+        assertTrue(schema.isValid("I like tests"));
+    }
+
+    @Test
+    void containsTest() {
+        schema.contains("li");
+
+        assertFalse(schema.isValid("bad"));
+
+        assertTrue(schema.isValid(null));
+        assertTrue(schema.isValid("I like tests"));
+    }
+
+    @Test
+    void CombinationOfVerificationTest() {
+        schema.minLength(7).contains("go");
+
+        assertTrue(schema.isValid(null));
+        assertFalse(schema.isValid("google"));
+
+        schema.required();
+        assertFalse(schema.isValid(null));
+        assertFalse(schema.isValid("no argument from 'contains'"));
+        assertTrue(schema.isValid("there is an argument 'go' from 'contains'"));
+
+        schema.minLength(4);
+        assertTrue(schema.isValid("google"));
+    }
+}
