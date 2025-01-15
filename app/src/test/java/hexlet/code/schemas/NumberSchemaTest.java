d810491d248a32e33246595f18db3a98351d1d47
sergeycherkasovv <iamcherkasov.job@gmail.com>
2025-01-15 20:07:07 +0700

    add NumberSchemaTest.java

---
+++ app/src/test/java/hexlet/code/schemas/NumberSchemaTest.java
@@ -0,0 +1,66 @@
+package hexlet.code.schemas;
+
+import hexlet.code.Validator;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class NumberSchemaTest {
+    NumberSchema schema;
+
+    @BeforeEach
+    void beforeEach() {
+        var valid = new Validator();
+        schema = valid.number();
+    }
+
+    @Test
+    void requiredTest() {
+        assertTrue(schema.isValid(null));
+
+        schema.required();
+
+        assertFalse(schema.isValid(null));
+        assertTrue(schema.isValid(1));
+    }
+
+    @Test
+    void positiveTest() {
+        schema.positive();
+
+        assertFalse(schema.isValid(-1));
+        assertFalse(schema.isValid(0));
+
+        assertTrue(schema.isValid(null));
+        assertTrue(schema.isValid(1));
+    }
+
+    @Test
+    void rangeTest() {
+        schema.range(-5, 5);
+
+        assertFalse(schema.isValid(-6));
+        assertFalse(schema.isValid(25));
+
+        assertTrue(schema.isValid(null));
+        assertTrue(schema.isValid(-1));
+        assertTrue(schema.isValid(0));
+        assertTrue(schema.isValid(5));
+        assertTrue(schema.range(5, 5).isValid(5));
+    }
+
+    @Test
+    void CombinationOfVerificationTest() {
+        schema.positive().range(-1, 1);
+
+        assertTrue(schema.isValid(null));
+        assertFalse(schema.isValid(-1));
+        assertTrue(schema.isValid(1));
+
+        schema.required();
+        assertFalse(schema.isValid(null));
+        assertFalse(schema.isValid(2));
+    }
+}
