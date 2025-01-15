cdf80b07c5b67dab70789f1746da8507c6388e67
sergeycherkasovv <iamcherkasov.job@gmail.com>
2025-01-15 20:08:03 +0700

    add BaseSchema.java

---
+++ app/src/main/java/hexlet/code/schemas/BaseSchema.java
@@ -0,0 +1,30 @@
+package hexlet.code.schemas;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.function.Predicate;
+
+public class BaseSchema<T> {
+    private Map<String, Predicate<T>> validations = new HashMap<>();
+
+    protected void addValidation(String key, Predicate<T> validation) {
+        this.validations.put(key, validation);
+    }
+
+    boolean isValid(T value) {
+        if (validations.containsKey("required") && value == null) {
+            return false;
+        }
+        if (value == null) {
+            return true;
+        }
+
+        return validations.values().stream().allMatch(t -> t.test(value));
+    }
+
+    BaseSchema<T> required() {
+        addValidation("required", Objects::nonNull);
+        return this;
+    }
+}
