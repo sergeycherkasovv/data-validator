ab0699876c3094f679124e8e02d962afb65630b7
sergeycherkasovv <iamcherkasov.job@gmail.com>
2025-01-18 17:14:32 +0700

    deleted StringSchema

---
+++ app/src/main/java/hexlet/code/schemas/StringSchema.java
@@ -0,0 +1,21 @@
+package hexlet.code.schemas;
+
+import java.util.Objects;
+
+public final class StringSchema extends BaseSchema<String> {
+
+    StringSchema required() {
+        addValidation("required", Objects::nonNull);
+        return this;
+    }
+
+    StringSchema minLength(int minSize) {
+        addValidation("minLength", s -> s.length() >= minSize);
+        return this;
+    }
+
+    StringSchema contains(String contain) {
+        addValidation("contains", s -> s.contains(contain));
+        return this;
+    }
+}
