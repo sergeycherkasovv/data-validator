62f80ee6adfc1f05839c4ba5353e3a516712bcbd
sergeycherkasovv <iamcherkasov.job@gmail.com>
2025-01-24 19:38:19 +0700

    deleted method 'required()'

--- app/src/main/java/hexlet/code/schemas/NumberSchema.java
+++ app/src/main/java/hexlet/code/schemas/NumberSchema.java
@@ -1,13 +1,7 @@
 package hexlet.code.schemas;
 
-import java.util.Objects;
 
 public final class NumberSchema extends BaseSchema<Integer> {
-
-    public NumberSchema required() {
-        addValidation("required", Objects::nonNull);
-        return this;
-    }
 
     public NumberSchema range(int begin, int end) {
         addValidation("range", i -> begin <= i && i <= end);
