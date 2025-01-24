a1926d500804cfd257cd55a573212307fc80fff3
sergeycherkasovv <iamcherkasov.job@gmail.com>
2025-01-24 23:26:56 +0700

    add javadoc for method required()

--- app/src/main/java/hexlet/code/schemas/BaseSchema.java
+++ app/src/main/java/hexlet/code/schemas/BaseSchema.java
@@ -26,6 +26,15 @@ public abstract class BaseSchema<T> {
                 .allMatch(t -> t.test(value));
     }
 
+    /**
+     * Marks the schema as required, ensuring that the value being validated is not null.
+     * <p>
+     * This method adds a "required" validation to the schema, which checks that the provided value
+     * is not {@code null}. If the validation fails, the schema will indicate an error.
+     * </p>
+     *
+     * @return the current {@code BaseSchema} instance for method chaining.
+     */
     public BaseSchema required() {
         addValidation("required", Objects::nonNull);
         return this;
