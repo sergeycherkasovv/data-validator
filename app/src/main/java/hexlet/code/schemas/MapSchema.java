40cdc71ae4d93d663123925279acde375a437332
sergeycherkasovv <iamcherkasov.job@gmail.com>
2025-01-24 19:38:15 +0700

    deleted method 'required()'

--- app/src/main/java/hexlet/code/schemas/MapSchema.java
+++ app/src/main/java/hexlet/code/schemas/MapSchema.java
@@ -2,14 +2,8 @@ package hexlet.code.schemas;
 
 
 import java.util.Map;
-import java.util.Objects;
 
 public final class MapSchema extends BaseSchema<Map<?, ?>> {
-
-    public MapSchema required() {
-        addValidation("required", Objects::nonNull);
-        return this;
-    }
 
     public MapSchema sizeof(int limitSize) {
         addValidation("sizeof", m -> m.size() == limitSize);
